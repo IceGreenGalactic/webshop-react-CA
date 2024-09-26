d14d33300210f4997b006e512116ca7bbbf16ccd
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-09-26 12:21:03 +0200

    ads img to recipte

--- src/pages/Checkout/CheckoutSuccess.jsx
+++ src/pages/Checkout/CheckoutSuccess.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "../../App.styles";
+import { Image } from "./CheckoutSuccess.styles";
 
 const CheckoutSuccess = () => {
   const location = useLocation();
@@ -16,28 +17,35 @@ const CheckoutSuccess = () => {
   return (
     <div>
       <h1 className="text-center my-4 ">Payment Successful</h1>
-      <div className="col-8 col-lg-6 m-auto mt-5 card p-4 ">
+      <div className="col-8 col-lg-6 m-auto mt-5 card p-4">
         <div className="col-6 m-auto">
-          <h2 className="text-decoration-underline">Your Receipt</h2>
-          <ul className="list-unstyled ">
-            {" "}
-            {/* Added list-unstyled for better styling */}
+          <h3 className="text-decoration-underline mb-4">Your Receipt</h3>
+          <ul className="list-unstyled">
             {purchasedItems.length > 0 ? (
               purchasedItems.map((item, index) => (
-                <li className="mb-2" key={index}>
-                  <strong>{item.title}</strong>: $
-                  {item.discountedPrice || item.price}
-                  <div className="text-muted">
-                    Quantity: {item.quantity}
-                  </div>{" "}
-                  {/* Quantity under item title */}
+                <li className="mb-3" key={index}>
+                  <div className="d-flex align-items-start">
+                    <Image
+                      src={item.image.url}
+                      alt={item.image.alt}
+                      className="me-3"
+                     
+                    />
+                    <div>
+                      <strong>{item.title}</strong>: $
+                      {item.discountedPrice || item.price}
+                      <div className="text-muted">
+                        Quantity: {item.quantity}
+                      </div>
+                    </div>
+                  </div>
                 </li>
               ))
             ) : (
               <li>No items purchased.</li>
             )}
           </ul>
-          <h3>Total Price: ${totalPrice.toFixed(2)}</h3>{" "}
+          <h3>Total Price: ${totalPrice.toFixed(2)}</h3>
         </div>
         <Button
           onClick={() => navigate("/")}
