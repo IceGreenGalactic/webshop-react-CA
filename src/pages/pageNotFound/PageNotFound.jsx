59fc2a668435dbf15f1aa7bd3726d069417d4444
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-10-03 14:09:43 +0200

    page not found image update

--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -9,7 +9,7 @@ const PageNotFound = () => {
         <div className="mt-5 m-auto col-10 col-md-8 col-lg-6">
           <h1 className="mt-5">404 </h1>
           <h2>Page not found</h2>
-          <img className="col-10 m-auto" src={Image} alt="monster hiding" />
+          <img className="col-10 m-auto col-xl-6" src={Image} alt="monster hiding" />
         </div>
       </div>
     </div>
